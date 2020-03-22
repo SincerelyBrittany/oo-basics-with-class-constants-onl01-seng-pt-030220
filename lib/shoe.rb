08481cc232cb9fc47de1d006475da5dcdcd6bd90
SincerelyBrittany <BrittanyRussell1993@gmail.com>
2020-03-21 20:10:53 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,24 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_accessor :color, :size, :material, :condition, :brand
+  # attr_reader :brand
+
+  BRANDS = []
 
   def initialize(brand)
-    @brand = brand
-  end
+  @brand = brand
+  BRANDS << brand unless BRANDS.include?(brand)
+end
+
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+
+  # create the writer for genre and add the logic for the class constant
+  # def brand=(brand)
+  #     @brand = brand
+  #   BRANDS << brand
+  # end
 
 end
